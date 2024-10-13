c312c62b13b6ee0412b9787abfd181e1b3659134
Chajian <936796603@qq.com>
2024-10-13 18:04:12 +0800

    feat:add pf4j plugin framework

---
+++ DockerManager/src/main/java/com/viaas/docker/config/PluginConfiguration.java
@@ -0,0 +1,24 @@
+package com.viaas.docker.config;
+
+import com.viaas.docker.plugin.Greetings;
+import org.pf4j.spring.SpringPluginManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+/**
+ * add plugin
+ */
+//@Configuration
+public class PluginConfiguration {
+    @Bean
+    public SpringPluginManager pluginManager() {
+        return new SpringPluginManager();
+    }
+
+    @Bean
+    @DependsOn("pluginManager")
+    public Greetings greetings() {
+        return new Greetings();
+    }
+}
