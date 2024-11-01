f05367975dfc7e2b0707bb3362e3c686ea195813
asdzxc1 <1240757402@qq.com>
2024-11-01 09:41:33 +0800

    修改cetification模块启动类名字

--- VS-CertificationAuthority/src/main/java/com/viaas/certification/CertificationApplication.java
+++ VS-CertificationAuthority/src/main/java/com/viaas/certification/CertificationApplication.java
@@ -3,19 +3,17 @@ package com.viaas.certification;
 import com.viaas.certification.api.filter.FormAuthenticationFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.stereotype.Component;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 @SpringBootApplication
 @MapperScan(basePackages = "com.viaas.certification.mapper")
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = FormAuthenticationFilter.class))
-public class Main {
+public class CertificationApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(CertificationApplication.class, args);
     }
 }
